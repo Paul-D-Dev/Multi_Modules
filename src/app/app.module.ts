38bec46ff72ed8ffb0e107ef0f0c4dae499bf330
Paul DAO <dao.van.paul@gmail.com>
2020-01-21 09:41:06 +0100

    imports multimodule Auth & Article in AppModule's imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { ArticleModule } from './pages/article/article.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './pages/auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    ArticleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
