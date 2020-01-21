6c7a8b8c90c6a24a7735c7523f1f56ed0b7a4fdf
Paul DAO <dao.van.paul@gmail.com>
2020-01-21 12:36:22 +0100

    manage component all routes

--- src/app/pages/manage/manage-routing.module.ts
+++ src/app/pages/manage/manage-routing.module.ts
@@ -1,9 +1,17 @@
+import { CreatePlaylistComponent } from './create-playlist/create-playlist.component';
+import { PlaylistDetailsComponent } from './playlist-details/playlist-details.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ManageComponent } from './manage.component';
 
-const routes: Routes = [{ path: '', component: ManageComponent }];
+const routes: Routes = [
+  { path: '', component: ManageComponent },
+  { path: 'playlists', component: PlaylistsComponent },
+  { path: 'playlist-details', component: PlaylistDetailsComponent },
+  { path: 'create-playlist', component: CreatePlaylistComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/manage/manage.module.ts
+++ src/app/pages/manage/manage.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ManageRoutingModule } from './manage-routing.module';
 import { ManageComponent } from './manage.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { PlaylistDetailsComponent } from './playlist-details/playlist-details.component';
+import { CreatePlaylistComponent } from './create-playlist/create-playlist.component';
 
 
 @NgModule({
-  declarations: [ManageComponent],
+  declarations: [ManageComponent, PlaylistsComponent, PlaylistDetailsComponent, CreatePlaylistComponent],
   imports: [
     CommonModule,
     ManageRoutingModule
