b7bd6af4b6f995cb99fb8781cd29e5b061cb0762
Paul DAO <dao.van.paul@gmail.com>
2020-01-21 11:09:31 +0100

    Generate module Auth & Manage with lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'manage', loadChildren: () => import('./pages/manage/manage.module').then(m => m.ManageModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
